a78ee24ba73c5ae84437124c6c1ca493e05a77c9
kamimi01 <awakaru0122@gmail.com>
2022-04-24 16:45:13 +0900

    refactor: メソッドの切り出し

--- swift/Tennis/TennisGame1.swift
+++ swift/Tennis/TennisGame1.swift
@@ -23,51 +23,69 @@ class TennisGame1: TennisGame {
     
     var score: String? {
         var score = ""
-        var tempScore = 0
         if score1 == score2 {
-            switch score1 {
-            case 0:
-                score = "Love-All"
-            case 1:
-                score = "Fifteen-All"
-            case 2:
-                score = "Thirty-All"
-            default:
-                score = "Deuce"
-            }
+            score = getResultWhenBothHaveSameScore(of: score1)
         } else if score1 >= 4 || score2 >= 4 {
-            let minusResult = score1 - score2
-            if minusResult == 1 {
-                score = "Advantage player1"
-            } else if minusResult  == -1 {
-                score = "Advantage player2"
-            } else if minusResult >= 2 {
-                score = "Win for player1"
-            } else {
-                score = "Win for player2"
-            }
+            score = getResultWhenOneSideHasMoreThan3(score1: score1, score2: score2)
         } else {
-            for i in 1..<3 {
-                if i == 1 {
-                    tempScore = score1
-                }
-                else {
-                    score = "\(score)-"
-                    tempScore = score2
-                }
+            score = getResultWhen(score1: score1, score2: score2)
+        }
+        return score
+    }
 
-                switch tempScore {
-                case 0:
-                    score = "\(score)Love"
-                case 1:
-                    score = "\(score)Fifteen"
-                case 2:
-                    score = "\(score)Thirty"
-                case 3:
-                    score = "\(score)Forty"
-                default:
-                    break
-                }
+    // スコアによってがスコアが同じ時のスコアボードに表示する結果を決める
+    private func getResultWhenBothHaveSameScore(of score: Int) -> String {
+        switch score {
+        case 0:
+            return "Love-All"
+        case 1:
+            return "Fifteen-All"
+        case 2:
+            return "Thirty-All"
+            // 各プレイヤーが少なくとも3ポイントを獲得し、スコアが等しい場合
+        default:
+            return "Deuce"
+        }
+    }
+
+    // 各サイドで少なくとも3ポイントが獲得され、プレーヤーが対戦相手より1ポイント多い場合
+    private func getResultWhenOneSideHasMoreThan3(score1: Int, score2: Int) -> String {
+        let minusResult = score1 - score2
+        if minusResult == 1 {
+            return "Advantage player1"
+        } else if minusResult  == -1 {
+            return "Advantage player2"
+        } else if minusResult >= 2 {
+            return "Win for player1"
+        } else {
+            return "Win for player2"
+        }
+    }
+
+    // FIXME: 良いメソッド名が思いつかず。そもそもこういうメソッドの切り方でよかったのか？
+    private func getResultWhen(score1: Int, score2: Int) -> String {
+        var score = ""
+        var tempScore = 0
+        for i in 1..<3 {
+            if i == 1 {
+                tempScore = score1
+            }
+            else {
+                score = "\(score)-"
+                tempScore = score2
+            }
+
+            switch tempScore {
+            case 0:
+                score = "\(score)Love"
+            case 1:
+                score = "\(score)Fifteen"
+            case 2:
+                score = "\(score)Thirty"
+            case 3:
+                score = "\(score)Forty"
+            default:
+                break
             }
         }
         return score
