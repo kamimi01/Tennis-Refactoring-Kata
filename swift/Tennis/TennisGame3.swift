aef998f45007e1559f46cb84f104c6bb9c806ee7
kamimi01 <awakaru0122@gmail.com>
2022-04-24 16:51:47 +0900

    refactor: 変数名の修正

--- swift/Tennis/TennisGame3.swift
+++ swift/Tennis/TennisGame3.swift
@@ -1,39 +1,38 @@
 import Foundation
 
 class TennisGame3: TennisGame {
-    private var p1: Int
-    private var p2: Int
-    private var p1N: String
-    private var p2N: String
+    private var player1: String
+    private var player2: String
+    private var score1: Int
+    private var score2: Int
     
     required init(player1: String, player2: String) {
-        p1N = player1
-        p2N = player2
-        p1 = 0
-        p2 = 0
+        self.player1 = player1
+        self.player2 = player2
+        score1 = 0
+        score2 = 0
     }
     
     var score: String? {
-        var s: String
-        if (p1 < 4 && p2 < 4) && (p1 + p2 < 6) {
-            let p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[p1]
-            return (p1 == p2) ? "\(s)-All" : "\(s)-\(p[p2])"
+        var score: String
+        if (score1 < 4 && score2 < 4) && (score1 + score2 < 6) {
+            let point = ["Love", "Fifteen", "Thirty", "Forty"]
+            score = point[score1]
+            return (score1 == score2) ? "\(score)-All" : "\(score)-\(point[score2])"
         } else {
-            if (p1 == p2) {
+            if (score1 == score2) {
                 return "Deuce"
             }
-            s = p1 > p2 ? p1N : p2N
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage \(s)" : "Win for \(s)"
+            score = score1 > score2 ? player1 : player2
+            return ((score1 - score2) * (score1 - score2) == 1) ? "Advantage \(score)" : "Win for \(score)"
         }
     }
     
     func wonPoint(_ playerName: String) {
         if playerName == "player1" {
-            p1 += 1
+            score1 += 1
         } else {
-            p2 += 1
+            score2 += 1
         }
     }
-    
 }
